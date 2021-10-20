add90590e3a430bdacabff2c0577da645fc9d80d
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2021-10-20 12:01:59 -0600

    Complete Part 3 model_b.rb

---
+++ models/model_b.rb
@@ -0,0 +1,11 @@
+# Code hash for the worlds best vehicle.
+worlds_best_vehicle = {
+  name_of_company => "Eddison",
+  type_of_car => "Electric",
+  number_of_factories => 3,
+  cars_per_year => 1500,
+  employees_needed => true,
+  job_requires_degree => false,
+  model_names => ["Atom", "Electron", "Nuetron"],
+  model_class => ["compact", "suv", "truck"]
+}
