4e8f9c28af7084f946363ede54d5bf1d69400721
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2021-10-20 11:54:56 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,22 @@
+# include puts so that this program can run on terminal with the
+# $ ruby variables.rb command
+famous_person = "Joe Sakic"
+
+is_active = true
+
+famous_person = "Peter Forsberg"
+
+puts famous_person
+
+num_3 = 4
+num_9 = 7
+
+puts sum = num_3 + num_9
+
+animals = ['cat', 'chicken', 'cow']
+
+animals.delete('cat')
+
+puts animals
+
+puts famous_person.size
