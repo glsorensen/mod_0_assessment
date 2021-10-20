e7aa48c618f6488ece685645d21ae98e170acc07
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2021-10-20 11:58:53 -0600

    Complete Part 3 model_a.rb

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+# example of a hash for AIRPORT.
+worlds_best_airport = {
+  airport_name => "Sorensen International",
+  nearest_city => "Roswell, NM",
+  number_of_runways => 3,
+  number_of_gates => 5,
+  open_year_around => true,
+  closed_on_christmas => false,
+  types_of_aircraft => ['747', 'A380', 'DC-10'],
+  airline_companies => ['Delta', 'American', 'Southwest']
+}
